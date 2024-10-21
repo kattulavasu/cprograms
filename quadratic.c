55e8cce5ecfe42d6bf29ea931ada18f602f69e2f
vasu <vasukattula428@gmail.com>
2024-10-21 12:15:49 +0530

    Create quadratic.c

---
+++ quadratic.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int a,b,c,d;
+    float r1,r2;
+    printf("enter a,b,c:");
+    scanf("%d%d%d",&a,&b,&c);
+    d=((b*b)- (4*a*c));
+    r1=(-b+sqrt(d)/2*a);
+    r2=(+b+sqrt(d)/2*a);
+    if(d>0)
+    {
+        printf("%f%f",&r1,&r2);
+    }
+    else if
+    (d<0)
+    {
+        printf("roots are imaginary");
+    }
+    else
+    {
+        printf("roots are real and equal");
+    }
+    }
