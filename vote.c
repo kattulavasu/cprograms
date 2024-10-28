517bd0691c30baf0601d43fa911ff159adbb35b8
vasu <vasukattula428@gmail.com>
2024-10-28 11:45:07 +0530

    Create vote.c

---
+++ vote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int a;
+    printf("enter age:");
+    scanf("%d",&a);
+    if(a>=18)
+    {
+        printf("eligible for vote");
+    }
+    else;
+    {
+        printf("not eliglible for vote");
+    }
+}
